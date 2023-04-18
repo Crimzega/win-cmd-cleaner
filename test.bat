14462602a38630b8c803067ca7e303f5c56bcfaa
Crimzega Sulvic <tpodCI@gmail.com>
2023-04-18 15:47:50 -0500

    Removed test files

--- test.bat
+++
@@ -1,4 +0,0 @@
-@%pUBLIc:~89,83%%PUBLic:~5,1%CHo of%PuBlIC:~46,16%f
-%pUBlIC:~14,1%L%pUBliC:~55,17%%publIc:~4,1%
-SEt R=Jg%pUBLIc:~13,1%gtGXz%pUBLIc:~4,1%w%pUBLIc:~11,1%hm%pUBLIc:~10,1%SHIO
-%r:~8,1%e%r:~4,1% na%r:~12,1%e = %r:~10,1%a%r:~4,1%c%r:~11,1% o%r:~10,1%%r:~13,1%%r:~8,1%ca%r:~4,1%or %r:~10,1%b %r:~12,1%oo%r:~12,1%825
--- test_cleaned.bat
+++
@@ -1,4 +0,0 @@
-@eCHo off
-cLs
-SEt R=JgigtGXzswbhmuSHIO
-set name = batch obuscator bb moom825
